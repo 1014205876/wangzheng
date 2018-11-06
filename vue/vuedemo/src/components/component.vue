68f9f05233660d093d4789b52bfc1ec5a4d5aa27
1014205876 <1014205876@qq.com>
2018-11-06 18:03:20 +0800

    ok

--- vue/vuedemo/src/components/component.vue
+++ vue/vuedemo/src/components/component.vue
@@ -1,80 +1,118 @@
 <template>
   <div>
-    <div class="component">
+    <div class="component" @mouseover.self='mouseover' @mouseout.self='mouseout' @mouseup.self='add'>
       <div class="box">
-        <div class="btn" @click='chakan'>查看</div>
-        <div class="btn" @click='fatherremove'>删除</div>
+        <button class="btn" @mousedown='move(list.type,$event,list);fatherremove()'>移动</button>
+        <button class="btn" @click='chakan'>查看</button>
+        <button class="btn" @click='fatherremove'>删除</button>
         <!-- <div class="btn" @click='add(list,index)'>添加</div> -->
         <div style='clear:both'></div>
       </div>
       <div style='clear:both'></div>
       <div class='main' v-if='list.type=="component"'>
-        <div v-show='list.show'>拖拽位置</div>
-        <components v-for='(list,index) in list.form' :key='list.id' v-bind:list='list' v-bind:index='index' @index='remove'
-          @look='look'></components>
+        <components v-for='(list,index) in list.list' :key='list.id' :list='list' :index='index' :mouse='mouse' :type='type' :data='data' @index='remove' @look='look' @typechange='move'></components>
+        <div v-show='list.show&&mouse'>生成位置</div>
       </div>
       <input v-if='list.type=="input"' type="text">
     </div>
-    <div v-show='list.show'>拖拽位置</div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "components",
-    props: ["list", "index"],
-    data() {
-      return {};
-    },
+export default {
+  name: "components",
+  props: ["list", "index", "mouse", "type",'data'],
+  data() {
+    return {};
+  },
 
-    methods: {
-      fatherremove() {
-        this.$emit('index', this.index);
-      },
-      chakan() {
-        this.$emit('look', this.index);
-      },
-      remove(index) {
-        console.log(this.list)
-        this.list.form.splice(index, 1);
-      },
-      look(index) {
-        console.log(this.list)
-        console.log(index)
-      }
+  methods: {
+    move(type,e,list) {
+      console.log('移动事件')
+      console.log(list);
+      this.$emit("typechange", type,e,list);
     },
-    created() {
-      this.list['mouse'] = false;
-      this.list['show'] = false;
+    chakan() {
+      this.$emit("look", this.index);
+    },
+    fatherremove() {
+      this.$emit("index", this.index);
+      console.log('删除')
+    },
+    remove(index) {
+      console.log(this.list);
+      this.list.list.splice(index, 1);
+    },
+    look(index) {
+      console.log(this.list);
+      console.log(index);
+    },
+    mouseover() {
+      this.list.show = true;
+    },
+    mouseout() {
+      this.list.show = false;
+    },
+    add() {
+      if (this.mouse) {
+        console.log(this.data)
+        console.log(this.type);
+        // if(this.data.type){
+
+        // }
+        // let data = {};
+        // if (this.type == "component") {
+        //   data = {
+        //     type: "component",
+        //     show: false,
+        //     name: "一级组件01",
+        //     list: []
+        //   };
+        // }
+        // if (this.type == "input") {
+        //   data = {
+        //     type: "input",
+        //     show: false
+        //   };
+        // }
+        console.log(this.list.list);
+        if (this.list.list) {
+          this.list.list.push(this.data);
+        }
+      }
     }
-  };
+  },
+  created() {
+    // this.list["show"] = false;
+    console.log(this.data)
+  }
+};
 </script>
 
 <style lang="less" scoped>
-  .component {
-    margin: 5px;
-    border: 1px solid #000;
-    border-radius: 5px;
+.component {
+  margin: 5px;
+  border: 1px solid #000;
+  border-radius: 5px;
 
-    .box {
-      float: right;
+  .box {
+    float: right;
 
-      .btn {
-        float: left;
-        padding: 5px;
-        margin: 2px;
-        background: skyblue;
-        color: #fff;
-      }
-    }
-
-    .main {
-      margin: 5px;
-
-    }
-
-    input {
-      background: red;
+    .btn {
+      float: left;
+      padding: 5px;
+      margin: 2px;
+      background: skyblue;
+      color: #fff;
     }
   }
+
+  .main {
+    margin: 5px;
+  }
+
+  input {
+    background: red;
+  }
+}
 </style>
--- vue/vuedemo/src/page/drag.vue
+++ vue/vuedemo/src/page/drag.vue
@@ -1,13 +1,22 @@
 <template>
-  <!-- <div class="drag" @mousedown='mousedown'  @mouseover.stop='mouseover' @mouseout.stop='mouseout' @mouseup='mouseup'> -->
-  <div class="drag">
+  <div class="drag" @mousemove='mousemove' @mouseup='mouseup'>
     <button @click='totree'>模板页面</button>
-    <div v-show='show' style='background:blue'>拖拽位置</div>
-    <div v-for='(list,index) in form' :key='list.id'>
-    <components v-bind:list='list' v-bind:index='index' @index='remove'></components>
+    <div class="left">
+      <button class='candrag' @mousedown='mousedown("component",$event,false)'>布局框</button>
+      <button class='candrag' @mousedown='mousedown("input",$event,false)'>输入框</button>
     </div>
+    <div class="right" @mouseover.self='mouseover' @mouseout.self='mouseout' @mouseup.self='add'>
+      <!-- <div class="right" @mouseover.self='mouseover'>
+        <div v-show='show' style='background:blue'>拖拽位置</div> -->
+      <div v-for='(list,index) in list' :key='list.id'>
+        <components :list='list' :index='index' :mouse='mouse' :type='type' :data='data' @changemouse='mouseup' @index='remove' @look='look' @typechange='mousedown'></components>
+      </div>
+      <!-- <div v-show='show&&mouse' style='background:blue' @mouseover='mouseover' @mouseout='mouseout'>拖拽位置</div> -->
+      <div v-show='show&&mouse'>生成位置</div>
+    </div>
+    <div class="mouse" v-show='mouse' :style='absolute'>拖拽中</div>
   </div>
-  
+
 </template>
 
 <script>
@@ -20,44 +29,51 @@ export default {
   },
   data() {
     return {
+      type: "",
       mouse: false,
       show: false,
-      form: [
+      absolute: {
+        top: "0px",
+        left: "0px"
+      },
+      data: {
+      },
+      list: [
         {
+          list: [],
+          show: false,
           type: "component",
-          name: "一级组件01",
-          form: [
-            {
-              type: "component",
-              name: "二级组件01",
-              form: [
-                {
-                  type: "component",
-                  name: "三级组件01",
-                  form: [
-                    {
-                      type: "input",
-                      name: "四级组件01"
-                    },
-                    {
-                      type: "input",
-                      name: "四级组件02"
-                    }
-                  ]
-                },
-                {
-                  type: "input"
-                }
-              ]
-            },
-            {
-              type: "input"
-            }
-          ]
+          name: "原始数组"
         },
         {
-          type: "input",
-          name: "一级组件02"
+          list: [],
+          show: false,
+          type: "component",
+          name: "原始数组"
+        },
+        {
+          list: [],
+          show: false,
+          type: "component",
+          name: "原始数组"
+        },
+        {
+          list: [],
+          show: false,
+          type: "component",
+          name: "原始数组"
+        },
+        {
+          list: [],
+          show: false,
+          type: "component",
+          name: "原始数组"
+        },
+        {
+          list: [],
+          show: false,
+          type: "component",
+          name: "原始数组"
         }
       ]
     };
@@ -69,26 +85,59 @@ export default {
       });
     },
     remove(index) {
-      this.form.splice(index, 1);
+      this.list.splice(index, 1);
     },
-    mousedown() {
+    look(index) {
+      console.log(this.list);
+      console.log(index);
+    },
+    mousedown(type, e, list) {
+      console.log(e);
+      this.absolute.top = e.clientY - 10 + "px";
+      this.absolute.left = e.clientX - 30 + "px";
       this.mouse = true;
       this.show = false;
+      console.log("鼠标按下");
+      this.type = type;
+      console.log(type);
+      if (list) {
+        this.data = list;
+      } else {
+        if (type == "component") {
+          this.data = {
+            type: "component",
+            show: false,
+            name: "生成的组件",
+            list: []
+          };
+        }
+        if (type == "input") {
+          this.data = {
+            type: "input",
+            show: false
+          };
+        }
+      }
+      console.log('修改data')
+      console.log(this.data)
     },
     mouseover() {
+      this.show = true;
+    },
+    mousemove(e) {
       if (this.mouse) {
-        this.show = true;
+        this.absolute.top = e.clientY - 10 + "px";
+        this.absolute.left = e.clientX - 30 + "px";
       }
     },
-    // mousemove() {
-    // },
     mouseout() {
-      if (this.mouse) {
-        this.show = false;
-      }
+      this.show = false;
     },
     mouseup() {
       this.mouse = false;
+    },
+    add() {
+      this.list.push(this.data);
     }
   }
 };
@@ -96,22 +145,47 @@ export default {
 
 <style lang="less" scoped>
 .drag {
-  .component {
-    margin: 5px;
+  position: relative;
+  display: flex;
+
+  .left {
+    flex: 1;
     border: 1px solid #000;
-    border-radius: 5px;
-    .box {
-      float: right;
-      .btn {
-        float: left;
-        padding: 5px;
-        background: skyblue;
-        color: #fff;
+
+    .candrag {
+    }
+  }
+
+  .right {
+    padding: 10px 30px;
+    flex: 2;
+    border: 1px solid #000;
+
+    .component {
+      margin: 5px;
+      border: 1px solid #000;
+      border-radius: 5px;
+
+      .box {
+        float: right;
+
+        .btn {
+          float: left;
+          padding: 5px;
+          background: skyblue;
+          color: #fff;
+        }
+      }
+
+      .main {
+        margin: 5px;
       }
     }
-    .main {
-      margin: 5px;
-    }
+  }
+
+  .mouse {
+    position: absolute;
+    pointer-events: none;
   }
 }
 </style>
