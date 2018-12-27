1ea9a2af617fa28d6f40e5dab9432d1219878d2e
1014205876 <1014205876@qq.com>
2018-12-27 16:38:51 +0800

    ok

--- angular/able/src/app/app-routing.module.ts
+++ angular/able/src/app/app-routing.module.ts
@@ -21,30 +21,30 @@ const routes: Routes = [
         path: 'application',
         loadChildren: './theme/application/application.module#ApplicationModule'
       },
-      {
-        path: 'configure',
-        loadChildren: './theme/configure/configure.module#ConfigureModule'
-      },
-      {
-        path: 'role',
-        loadChildren: './theme/role/role.module#RoleModule'
-      },
-      {
-        path: 'organization',
-        loadChildren: './theme/organization/organization.module#OrganizationModule'
-      },
-      {
-        path: 'baseResource',
-        loadChildren: './theme/base-resource/base-resource.module#BaseResourceModule'
-      },
-      {
-        path: 'businessResource',
-        loadChildren: './theme/business-resource/business-resource.module#BusinessResourceModule'
-      },
-      {
-        path: 'log',
-        loadChildren: './theme/log/log.module#LogModule'
-      }
+      // {
+      //   path: 'configure',
+      //   loadChildren: './theme/configure/configure.module#ConfigureModule'
+      // },
+      // {
+      //   path: 'role',
+      //   loadChildren: './theme/role/role.module#RoleModule'
+      // },
+      // {
+      //   path: 'organization',
+      //   loadChildren: './theme/organization/organization.module#OrganizationModule'
+      // },
+      // {
+      //   path: 'baseResource',
+      //   loadChildren: './theme/base-resource/base-resource.module#BaseResourceModule'
+      // },
+      // {
+      //   path: 'businessResource',
+      //   loadChildren: './theme/business-resource/business-resource.module#BusinessResourceModule'
+      // },
+      // {
+      //   path: 'log',
+      //   loadChildren: './theme/log/log.module#LogModule'
+      // }
     ]
   }
 ];
