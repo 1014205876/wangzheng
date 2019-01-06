a53cf1309c787ceafac24cbe20e76e04b3eaaf47
1014205876@qq.com <1014205876@qq.com>
2019-01-06 15:26:33 +0800

    ok

--- angular/zorro/src/app/pages/home/home.component.ts
+++ angular/zorro/src/app/pages/home/home.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
+
+  constructor() { }
+  //生命变量
   data = [
     {
       parentId: '',
@@ -43,14 +46,41 @@ export class HomeComponent implements OnInit {
       address: 'London No. 1 Lake Park',
     }
   ];
+  data1 = [
+    {
+      parentId: '',
+      id: 1,
+      name: '一级1',
+      age: 60,
+      address: 'New York No. 1 Lake Park',
+    },
+    {
+      parentId: 1,
+      id: 11,
+      name: '一级1-二级1',
+      age: 42,
+      address: 'New York No. 2 Lake Park'
+    },
+    {
+      parentId: 11,
+      id: 111,
+      name: '一级1-二级1-三级1',
+      age: 30,
+      address: 'New York No. 3 Lake Park'
+    }
+  ];
   dataTable = [];
-  expandDataCache = {};
-
-  collapse(array, data, $event: boolean): void {
+  // 触发函数
+  consol(arr) {
+    arr.forEach(item => {
+      console.log(item)
+    })
+  }
+  collapse(array, data, $event: boolean): void {//点击展开收起时执行
     if ($event === false) {
       if (data.children) {
         data.children.forEach(d => {
-          const target = array.find(a => a.id === d.id);
+          const target = array.find(a => a.data.id === d.id);
           target.expand = false;
           this.collapse(array, target, false);
         });
@@ -59,74 +89,73 @@ export class HomeComponent implements OnInit {
       }
     }
   }
-
-  convertTreeToList(root: object) {
-    const stack = [];
-    const array = [];
-    const hashMap = {};
-    stack.push({ ...root, level: 0, expand: false });
-
-    while (stack.length !== 0) {
-      const node = stack.pop();
-      this.visitNode(node, hashMap, array);
-      if (node.children) {
-        for (let i = node.children.length - 1; i >= 0; i--) {
-          stack.push({ ...node.children[i], level: node.level + 1, expand: false, parent: node });
+  openall(dataTable) {
+    dataTable.forEach(function (item) {
+      item.expand = true;
+    })
+  }
+  closeall(dataTable) {
+    dataTable.forEach(function (item) {
+      item.expand = false;
+    })
+  }
+  update() {
+    let that = this;
+    this.data = this.data1;
+    that.dataTable=that.getNewData(that, that.data)//将后台获取到的数据转换成前台展示的格式（添加部分属性控制树形表格的显示）
+  }
+  // 调用函数
+  getNewData(that, data) {
+    let dataTable = [];
+    data.forEach(item => {
+      let parent;//双向绑定，控制tr标签的显示隐藏(为dataTable中符合条件的数据)
+      if (item.parentId) {
+        if (dataTable.length) {
+          for (let i = 0; i < dataTable.length; i++) {
+            if (dataTable[i].data.id == item.parentId) {
+              parent = dataTable[i]
+            }
+          }
+        }
+      } else {
+        parent = {
+          expand: true
         }
       }
-    }
-
-    return array;
-  }
-
-  visitNode(node, hashMap: object, array): void {
-    if (!hashMap[node.id]) {
-      hashMap[node.id] = true;
-      array.push(node);
-    }
-  }
-  getchildren(that, data, id, children) {
-    let num = 0;
-    for (let i = 0; i < data.length; i++) {
-      if (id == data[i].parentId) {
-        console.log(data[i])
-        children.push(data[i])
-        // that.getchildren(that, data, data[i].id, children)
-      }
-    }
-    if (num == 0) {
-      children = null;
-    }
-  }
-  getdata(that, data) {
-    data.forEach(item => {
-      let parent;
+      //用于控制树形表格的缩进
+      let level;
       if (item.parentId) {
-        parent = data[data.indexOf(item.parentId)]
+        level = parent.level + 1
       } else {
-        parent = null
+        level = 0
       }
-      let children=[];
-      that.getchildren(that, data, item.id, children)
-      that.dataTable.push(
-        {
-          item,
-          expand: false,
-          loading: false,
-          parent: parent, children: children
+      // 添加属性children(为原始数据中符合条件的数据)
+      let children = [];
+      for (let i = 0; i < data.length; i++) {
+        if (item.id == data[i].parentId) {
+          children.push(data[i])
+        }
+      }
+      //将生成的数据赋值进dataTable
+      let obj = Object.assign(//合并对象
+        { data: item }, //原始data数据对象防止与后端重名
+        {//前端添加的对象，用于树形表格的展示
+          level: level,//控制树形表格缩进
+          expand: false,//控制树形表格的展开收起
+          loading: false,//控制树形表格switch的loading
+          parent: parent,//树形表格的展开收起
+          children: children,//控制树形表格的展开收起
         }
       )
+      dataTable.push(obj)
     });
+    return dataTable
   }
-consol(arr){
-  arr.forEach(item=>{
-    console.log(item)
-  })
-}
+
+  // 生命周期
   ngOnInit(): void {
     let that = this;
-    that.getdata(that, that.data)
-    console.log(that.dataTable)
+    that.dataTable=that.getNewData(that, that.data)//将后台获取到的数据转换成前台展示的格式（添加部分属性控制树形表格的显示）
   }
 
 }
