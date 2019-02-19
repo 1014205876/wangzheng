e35ed8478d69c02a34385fde0d8602a7285bb35f
1014205876 <1014205876@qq.com>
2019-02-19 18:08:59 +0800

    ok

--- node/server.js
+++ node/server.js
@@ -5,8 +5,8 @@ http.createServer(function (request, response) {
         200,
         { 'Content-Type': 'text/plain' }
     );
-    response.end('Hello World\n');
+    response.end('Hello World\n123');
 }).listen(8888);
 
 // 终端打印如下信息
-console.log('Server running at http://127.0.0.1:8888/');
+console.log('Server running at http://localhost:8888/');
