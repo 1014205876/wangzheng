2915565a92cd68636feedbcbef26a956d9c515c1
1014205876@qq.com <1014205876@qq.com>
2019-06-07 19:32:43 +0800

    ok

--- angular/kalanchoe-manager-new/manager/src/app/app-routing.module.ts
+++ angular/kalanchoe-manager-new/manager/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// 引入布局组件
+import { AdminComponent } from './layout/admin/admin.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminComponent
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
