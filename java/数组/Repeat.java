aaadab164d96a2fc0bd2a245db36031dd57e9faf
1014205876@qq.com <1014205876@qq.com>
2019-07-04 23:08:02 +0800

    java

--- java/数组/Repeat.java
+++ java/数组/Repeat.java
@@ -3,8 +3,11 @@ import java.util.Arrays;
 public class Repeat {
     public static void main(String[] args) {
         int arr[] = new int[] { 23, 42, 12, 6, 10 };
+        System.out.println(arr);
+        // Arrays.sort(arr);
+        // System.out.println(arr);
         int newarr[] = Arrays.copyOfRange(arr, 0, 3);
-        int newar = Arrays.binarySearch(arr, 6);
+        int newar = Arrays.binarySearch(arr, 42);
         System.out.println(newar);
         for (int i = 0; i < newarr.length; i++) {
             System.out.println(newarr[i]);
