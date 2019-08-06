a28092a33f2bad897dc26d2025a7d00a0599291a
1014205876@qq.com <1014205876@qq.com>
2019-08-07 00:00:51 +0800

    ok

--- java/数据库/Conn.java
+++ java/数据库/Conn.java
@@ -1,26 +1,28 @@
 import java.sql.*;
-public class Conn{
+
+public class Conn {
     Connection con;
-    public Connection getConnection(){
+
+    public Connection getConnection() {
         System.out.println("数据");
         try{
-            Class.forName("com.mysql.jdbc.Driver");
-            System.out.println("加载成功");
-        }catch(ClassNotFoundExcepttion e){
-            e.printStackTrace();
+        Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("加载成功");
+        }catch(ClassNotFoundException e){
+        e.printStackTrace();
         }
 
-        try{
-            con=DriverManager.getConnection("jdbc:mysql:"+"192.168.111.134'3306/wang","root","root");
-            
-        System.out.println("连接成功");
-        }catch(SQLException e){
+        try {
+            con = DriverManager.getConnection("jdbc:mysql:" + "//127.0.0.1:3306/wang", "root", "root");
+            System.out.println("连接成功");
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return con;
     }
-    public static void main(String[] args){
-        Conn c=new Conn();
+
+    public static void main(String[] args) {
+        Conn c = new Conn();
         c.getConnection();
     }
 }
