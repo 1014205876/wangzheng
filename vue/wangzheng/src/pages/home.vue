49bb8670af3df6dd48f656900e8b877ae2561ecc
wangzheng <wz@96325>
2019-09-02 14:19:57 +0800

    ok

---
+++ vue/wangzheng/src/pages/home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="home">123</div>
+</template>
+
+<script>
+    export default {
+        name: 'home',
+        components: {
+        }
+    }
+</script>
+
+<style>
+</style>
