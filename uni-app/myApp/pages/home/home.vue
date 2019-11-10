4b7ce5bfdf83a4740a24aee7074cd28f7d586e0e
1014205876@qq.com <1014205876@qq.com>
2019-11-10 10:55:01 +0800

    over

---
+++ uni-app/myApp/pages/home/home.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="header">
+		<view class="address">{{city}}</view>
+		<view class="input">
+			<input type="text" value="" />
+		</view>
+		<view class="find">
+			<button type="primary">查询</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		data(){
+			return {
+				city:'北京',
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
