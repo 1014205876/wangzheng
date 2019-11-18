ef86c240b5d5bf886ec27ca2628c6e182c6e608e
wangzheng <wz@96325>
2019-11-18 09:12:59 +0800

    ov

--- vue/vuedemo/config/index.js
+++ vue/vuedemo/config/index.js
@@ -11,8 +11,8 @@ module.exports = {
         assetsPublicPath: '/',
         proxyTable: {},
         // Various Dev Server settings
-        // host: 'localhost', // can be overwritten by process.env.HOST
-        host: '192.168.111.134', // can be overwritten by process.env.HOST
+        host: 'localhost', // can be overwritten by process.env.HOST
+        // host: '192.168.111.134', // can be overwritten by process.env.HOST
         // host: '192.168.1.2', // can be overwritten by process.env.HOST
         port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
         autoOpenBrowser: true,
